dd78ebfba53d0ae0e6b3170ff2553e33c33368b5
pr0gger01 <pr0gger.dev@gmail.com>
2024-09-29 00:26:25 +0300

    исправлен путь логов

--- recommendationApi/logger.py
+++ recommendationApi/logger.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
     level=logging.INFO,
-    filename="../src/main/resources/pythonApi/pylog.log"
+    filename="pylog.log"
 )
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
